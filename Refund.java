13ab8523784f142c068cf4c8bb7cdf1c8e9c23aa
dotdotot <dotdotot203@naver.com>
2021-12-10 16:13:23 +0900

    환불

--- Refund.java
+++ Refund.java
@@ -43,25 +43,18 @@ class Refund_Main extends JFrame {
 		refundButton.addMouseListener(new MouseAdapter() {
 			public void mousePressed(MouseEvent e) {
 				String transaction = transactionNumberTextField.getText();
-                boolean target = true;
-                
-                
-
+                boolean target = user.pos.productRefund(transaction);
 				// 환불 성공
 				if (target) {
-					// 장부에서 제외하고 수량을 줄이는 코드가 들어가야함
-
-					// ------------------------------------
-
-					// 환불 성공 폼
+					// 환불 성공 
+					JOptionPane.showMessageDialog(null, "환불이 되었습니다.");
 					setVisible(false);
-					new RefundSuccess();
+					new Main(user);
 				}
 				// 환불 실패
 				else {
-					// 환불 실패 폼
-					setVisible(false);
-					new RefundFail();
+					// 환불 실패 
+					JOptionPane.showMessageDialog(null, "환불번호가 틀렸습니다.");
 				}
 			}
 		});
@@ -73,62 +66,6 @@ class Refund_Main extends JFrame {
 		c.add(refundButton);
 		c.add(transactionNumberLabel);
 		c.add(transactionNumberTextField);
-
-		setSize(400, 200);
-		setVisible(true);
-	}
-}
-
-// TODO 환불 성공
-class RefundSuccess extends JFrame {
-	public RefundSuccess() {
-		// 제목 설정, 메인 스레드 종료시 이벤트 스레드도 종료하도록 설정
-		setTitle("환불 성공");
-
-		// 프레임을 화면 중앙에 배치
-		setLocationRelativeTo(null);
-
-		// Main컨테이너 이름을 c로 설정, 배치관리자 삭제, 백그라운드 설정
-		Container c = getContentPane();
-		c.setLayout(null);
-		// 일단 하얀색으로 배경색 설정, 추후 이미지로 바꿀 예정
-		c.setBackground(Color.WHITE);
-
-		Font textFont = new Font("이텔릭체", Font.ITALIC, 80);
-
-		JLabel success = new JLabel("성공!!");
-		success.setFont(textFont);
-		success.setBounds(80, 40, 250, 80);
-
-		c.add(success);
-
-		setSize(400, 200);
-		setVisible(true);
-	}
-}
-
-// TODO 환불 실패
-class RefundFail extends JFrame {
-	public RefundFail() {
-		// 제목 설정, 메인 스레드 종료시 이벤트 스레드도 종료하도록 설정
-		setTitle("환불 실패");
-
-		// 프레임을 화면 중앙에 배치
-		setLocationRelativeTo(null);
-
-		// Main컨테이너 이름을 c로 설정, 배치관리자 삭제, 백그라운드 설정
-		Container c = getContentPane();
-		c.setLayout(null);
-		// 일단 하얀색으로 배경색 설정, 추후 이미지로 바꿀 예정
-		c.setBackground(Color.WHITE);
-
-		Font textFont = new Font("이텔릭체", Font.ITALIC, 80);
-
-		JLabel success = new JLabel("실패..");
-		success.setFont(textFont);
-		success.setBounds(80, 40, 250, 80);
-
-		c.add(success);
 
 		setSize(400, 200);
 		setVisible(true);
